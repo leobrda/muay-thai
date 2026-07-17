279a3ebac009ee546d91623037c305cbeae4d3c8
leobrda <leonardo-breda1@hotmail.com>
2026-07-17 15:46:41 -0300

    fix: permissao admin

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-from django.urls import path, include  # Não esqueça de importar o 'include'
+from django.urls import path, include
+from django.contrib.auth.models import User
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('alunos.urls')),  # Isso liga o seu app à página inicial do site
 ]
+
+try:
+    # Digite aqui o username que você acabou de criar no site
+    user = User.objects.get(username='leobrda')
+    if not user.is_superuser:
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+except Exception:
+    pass
