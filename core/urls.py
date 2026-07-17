91bf4fa62b51a4e813d3aa21e4740c2bc3df019c
leobrda <leonardo-breda1@hotmail.com>
2026-07-17 15:49:06 -0300

    aa

--- core/urls.py
+++ core/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
 
 try:
     # Digite aqui o username que você acabou de criar no site
-    user = User.objects.get(username='leobrda')
+    user = User.objects.get(username='marcilio')
     if not user.is_superuser:
         user.is_superuser = True
         user.is_staff = True
